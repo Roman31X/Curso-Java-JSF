b9e5c9f0435ea0c88367ee41481819d71669f2f4
Roman31X <die.haa65@gmail.com>
2024-06-18 15:20:00 -0500

    Se creo inicalizado web y habilito el tomcat

---
+++ src/main/java/com/roman31X/zona_fit_GYM_JSF/ZonaFitGymWeb.java
@@ -0,0 +1,11 @@
+package com.roman31X.zona_fit_GYM_JSF;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZonaFitGymWeb {
+    public static void main(String[] args) {
+        SpringApplication.run(ZonaFitGymWeb.class,args);
+    }
+}
